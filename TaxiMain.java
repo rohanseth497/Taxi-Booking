0b6415530f7f3e39df1c88b01978ee607b8cffa4
rohanseth497 <rohan.seth497@gmail.com>
2021-08-19 00:40:41 +0530

    Validation for basic test cases | Completed

--- TaxiMain.java
+++ TaxiMain.java
@@ -14,24 +14,25 @@ public class TaxiMain {
         stationService.saveStation("A", 0, 0);
         stationService.saveStation("B", 15, 60);
         stationService.saveStation("C", 30, 120);
-        stationService.saveStation("D", 45, 180);
-        stationService.saveStation("E", 60, 240);
-        stationService.saveStation("F", 75, 300);
+        // stationService.saveStation("D", 45, 180);
+        // stationService.saveStation("E", 60, 240);
+        // stationService.saveStation("F", 75, 300);
 
         ITaxiService taxiService = TaxiServiceImpl.getInstance();
         taxiService.saveTaxi("1", "A");
         taxiService.saveTaxi("2", "B");
         taxiService.saveTaxi("3", "C");
-        taxiService.saveTaxi("4", "D");
-        taxiService.saveTaxi("5", "E");
+        // taxiService.saveTaxi("4", "D");
+        // taxiService.saveTaxi("5", "E");
 
         IBookingService bookingService = BookingServiceImpl.getInstance();
-        Boolean isBooked = bookingService.book("A", "B", new Date(2021, 8, 18, 11, 0));
-        if (isBooked) {
-            System.out.println("Booked");
-        } else {
-            System.out.println("Not Booked");
-        }
+        
+        bookingService.book("A", "B", new Date(2021, 8, 18, 11, 0));
+        bookingService.book("A", "C", new Date(2021, 8, 18, 11, 0));
+        bookingService.book("B", "C", new Date(2021, 8, 18, 11, 0));
+        bookingService.book("C", "A", new Date(2021, 8, 18, 14, 0));
+        bookingService.book("A", "C", new Date(2021, 8, 18, 11, 0));
+        bookingService.book("C", "A", new Date(2021, 8, 18, 15, 0));
 
     }
 }
